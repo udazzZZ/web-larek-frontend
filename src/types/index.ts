6dc90d304083cb0bf7194a0f034846c57b8d5a1c
udazzZZ <pirogova.dv96@mail.ru>
2025-05-20 08:51:16 +0300

    feat: add types

---
+++ src/types/index.ts
@@ -0,0 +1,42 @@
+// Товар
+
+export interface IProduct {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number | null;
+}
+
+// Тип оплаты
+
+export type TOrderPayment = 'cash' | 'card';
+
+// Заказ
+
+export interface IOrder {
+	items: IProduct[];
+	payment: TOrderPayment;
+	address: string;
+	email: string;
+	phone: string;
+}
+
+// Квитанция по заказу
+
+export type TOrderInvoice = Omit<IOrder, 'items'> & {
+	items: string[];
+	total: number;
+};
+
+// Шаги оформления заказа
+
+export type TOrderStep = 'shipment' | 'contacts';
+
+// Результат оформления заказа
+
+export interface IOrderResult {
+  id: string;
+  total: number;
+}
